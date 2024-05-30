691989ee53322f59996957f54f60b9540a4789f2
MJ <155809042+mjjung2@users.noreply.github.com>
2024-05-30 16:33:09 +0900

    Revert "Merge PR : merge 'minji' into 'dev'"

--- src/main/java/com/knowget/knowgetbackend/domain/successCase/repository/SuccessCaseRepository.java
+++ src/main/java/com/knowget/knowgetbackend/domain/successCase/repository/SuccessCaseRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.knowget.knowgetbackend.global.entity.SuccessCase;
 
 public interface SuccessCaseRepository extends JpaRepository<SuccessCase, Integer> {
-
 }
